3eb782b87e7f541195efc682b93058859dcdfbb7
ChrisM345 <chrism.12277@gmail.com>
2024-03-02 11:33:12 -0600

    Began work on including form for new books when clicking on Add Book button

--- js/scripts.js
+++ js/scripts.js
@@ -42,3 +42,14 @@ function displayBooks(){
 }
 
 const bookSection = document.querySelector(".book-container")
+const dialog = document.querySelector("dialog");
+const showButton = document.querySelector("dialog + button");
+const closeButton = document.querySelector("dialog button");
+
+showButton.addEventListener("click", () => {
+    dialog.showModal();
+});
+
+closeButton.addEventListener("click", () => {
+    dialog.close();
+});
